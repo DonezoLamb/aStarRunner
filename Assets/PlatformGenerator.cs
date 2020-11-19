b2ec675705b9b14b9007a9f376cf0aad5c146a1c
Robert Lamb <44955848+DonezoLamb@users.noreply.github.com>
2020-11-19 18:52:53 -0500

    Platform Spawn Optimization 1
    the platforms that the player runs on are now aligned with the A* grid

--- Assets/PlatformGenerator.cs
+++ Assets/PlatformGenerator.cs
@@ -34,6 +34,10 @@ public class PlatformGenerator : MonoBehaviour
         {
             SpawnPlatform();
         }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            SetNewSpawnPoint();
+        }
     }
 
     public void CallSpawner()
@@ -49,13 +53,20 @@ public class PlatformGenerator : MonoBehaviour
         newPlat.transform.localScale += new Vector3(1f,10f,0);
     }
 
+
+    //X axis aligns at .25 and .75
+    //Y axis aligns at .00 and .5
     public void SetNewSpawnPoint()
     {
         //set x within range
         curX += Random.Range(xOffsetMin,xOffsetMax);
+        curX = Mathf.Round(curX * 2f) * .5f + .25f;
+
         //set platform y to be similar to previous plat
         //TODO a line to make it more liekly to return to the middle after reaching an edge case
         curYOffset += Random.Range(-yOffsetDist, yOffsetDist);
+        curYOffset = Mathf.Round(curYOffset * 2f) * .5f;
+
         if(curYOffset>yOffsetMax)
         {
             curYOffset = yOffsetMax;
